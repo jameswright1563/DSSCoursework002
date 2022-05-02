f5ef0b9e80de1f965d4bb77c05c6d60bc548431b
james <jameswright1563@gmail.com>
2022-05-02 14:05:13 +0100

    Nav Bar changed

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@
 function likedislike(x) {
     x.classList.toggle("fa-thumbs-down");
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
