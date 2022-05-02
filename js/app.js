16b25cdbdc6ed720dba816e9c19f2c9036572b44
james <jameswright1563@gmail.com>
2022-05-02 13:43:19 +0100

    Added test html site and added notes

--- js/app.js
+++ js/app.js
@@ -1,34 +1,5 @@
+
+
 function likedislike(x) {
     x.classList.toggle("fa-thumbs-down");
 }
-
-function testFunction(){
-    console.log("Test Function")
-}
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-function mongoose() {
-    const {MongoClient, ServerApiVersion} = require('mongodb');
-    const dbURI = "mongodb+srv://js_user:4488@cluster0.jp9fi.mongodb.net/test?retryWrites=true&w=majority";
-    const client = new MongoClient(dbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1
-    });
-    client.connect(err => {
-        const collection = client.db("test").collection("blogs");
-        // perform actions on the collection object
-        client.close();
-    });
-
-}
-
-
