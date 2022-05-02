feafff29f5e8d704b97f4ba0391f4feebeeb572d
james <jameswright1563@gmail.com>
2022-05-02 14:55:14 +0100

    Added js use

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path')
 //these are the modules that are needed for the code to work
 
 
@@ -8,9 +9,19 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED='0';
 //this turns off SSL certificate checking, as we dont have a SSL certificate
 
 
-app.use(express.static('html'));//specifies where the static files are
-app.use('/img',express.static('img'));
-app.use('/css', express.static('css'))
+app.get('/', function(req, res){
+
+   res.sendFile(path.join(__dirname, '/html/index.html'))
+   app.use('/html',express.static('html'));
+
+   app.use('/img',express.static('img'));
+   app.use('/css', express.static('css'))
+   app.use('/js', express.static('js'))
+
+   // res.sendFile('img/')
+
+})
+
 var server = app.listen(3000, function () { //setup the server
    var host = server.address().address
    var port = server.address().port
