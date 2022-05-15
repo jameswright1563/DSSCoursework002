b15c20a9cdc7b7a23c5c7d5533ba3693253c785a
Outlands <elevatedlegendz@gmail.com>
2022-05-15 20:19:55 +0100

    wad

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookie_parser('1234'))
 let session = require('express-session')
 let File_Store = require('session-file-store')(session)
 const helmet = require("helmet");
-//app.use(helmet())
+app.use(helmet())
 //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
